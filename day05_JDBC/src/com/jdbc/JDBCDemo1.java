5f7843cddad85ab74914df3998a42c5d67e0ecc3
yjj1029 <924621887@qq.com>
2020-09-09 23:01:39 +0800

    day02_03_04_05_06。MySQL基础，MySQL约束，MySQL多表&事务，JDBC，JDBC连接池&JDBCTemplate。

---
+++ day05_JDBC/src/com/jdbc/JDBCDemo1.java
@@ -0,0 +1,35 @@
+/**
+ * @作者 鄢加军
+ * @我的学习 $ https://github.com/yjj1029/Java-ReStudy
+ * @想说的话 靠自己才能成功，自律才会成功！！！
+ * @创建时间 2020/9/9 17:04
+ */
+package com.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCDemo1 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        //1. 导入驱动jar包
+        //2.注册驱动
+        Class.forName("com.mysql.jdbc.Driver");
+        //3.获取数据库连接对象
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db3", "root", "root");
+        //4.定义sql语句
+        String sql = "update emp set salary = 500 where job_id = 2";
+        //5.获取执行sql的对象 Statement
+        Statement stmt = conn.createStatement();
+        //6.执行sql
+        int count = stmt.executeUpdate(sql);
+        //7.处理结果
+        System.out.println(count);
+        //8.释放资源
+        stmt.close();
+        conn.close();
+
+
+    }
+}
